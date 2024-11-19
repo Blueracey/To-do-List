51f4afac26a5fe2c7c67e4af71ec87e3a5d34356
Corbyn <144940013+Blueracey@users.noreply.github.com>
2024-11-18 23:17:43 -0700

    Initial commit
    Added basic to do list functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,103 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [name,setName] = useState('')
+  const [description,setDescription] = useState('')
+  const [tasks,setTasks] = useState([
+
+  ])
+
+
+
+
+  const Delete = ( name,description ) => {
+      
+    setTasks(tasks.filter(task => task.name !== name && task.description !== description));
+    
+
+  
+
+}
+
+  const handleSubmit = () =>{
+    
+
+    const newItem ={ name: name,
+    description: description 
+    }
+
+
+
+
+      const newTask = [...tasks,newItem];
+
+      setTasks(newTask)
+       
+  }
+    
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> To do list</h1>
+        
+
+
+        <div class ="InputArea">
+          
+
+
+          <input 
+          name = 'name'
+          placeholder='Name'
+          value={name}
+          onChange={(e) =>setName(e.target.value)}/>
+          <textarea 
+          name = 'description'
+          placeholder='Description'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          rows={10}
+          
+          ></textarea>
+          
+
+
+        <button type='submit' onClick={handleSubmit}>submit</button>
+
+
+          
+
+        </div>
+
+
+        
+        <ul>
+            {tasks.map((task,index) => 
+            <div key={index}>
+              <div class = "ListArea">
+              <div class ="TaskName">{task.name}<button onClick={() => Delete(task.name, task.description)} >Delete</button></div> 
+              
+               <div class = "TaskDescription">{task.description}</div>
+               </div>
+
+               
+            </div>
+            )}
+
+
+
+        </ul>
+
+
+        
+        
       </header>
     </div>
   );
